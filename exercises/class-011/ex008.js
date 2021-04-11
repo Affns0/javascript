bd97e79cabd2b38e005c5991ba2ab6018008cf2d
Pedro Afonso <apolaropeo@gmail.com>
2021-04-11 13:10:49 -0300

    fines system

---
+++ exercises/class-011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`Your car speed is ${vel}km/h`)
+if (vel >= 60) {
+    console.log('You axceeded the allowed speed< FINED!')
+}
+console.log('be careful drive safe')
+
---
+++ exercises/class-011/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Italy'
+console.log(`Livin in ${country}`)
+if (country == 'Brazil') {
+    console.log('You are brazilian')
+} else {
+    console.log('You are foreign')
+}
