107275ff1ee89eb4df416abe9816a2691d4910d2
Pedro Afonso <apolaropeo@gmail.com>
2021-05-05 14:33:16 -0300

    ex013

---
+++ exercises/class-013/class013.js
@@ -0,0 +1,34 @@
+
+/*
+var c = 1
+do {
+    console.log(`step ${c}`)
+    c++
+} while (c <= 8)
+*/
+
+
+
+
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`step ${c}`)
+    c++
+}
+*/
+
+
+
+
+
+
+/*
+console.log('are you okay')
+console.log('are you okay')
+console.log('are you okay')
+console.log('are you okay')
+console.log('are you okay')
+*/
