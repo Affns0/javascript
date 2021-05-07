87d25e364c221d4713730b1bc05427d8b1eb7902
Pedro Afonso <apolaropeo@gmail.com>
2021-05-07 18:34:30 -0300

    ex016

---
+++ exercises/class-016/functions02.js
@@ -0,0 +1,7 @@
+function soma(n1 = 0, n2 = 0) {
+    return n1 + n2 
+}
+
+console.log(soma(7))
+
+/*add 0 no parametro pq caso o usuario nao digitar um numero o codigo vai reconhecer como zero e nao vai dar erro o programa.*/
---
+++ exercises/class-016/functions03.js
@@ -0,0 +1,7 @@
+let v = function(x) {
+    return x * 2
+}
+
+console.log(v(5))
+
+/*Eu tbm posso color uma funcao dentro de uma variavel */
