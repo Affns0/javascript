5790d01d727e664187e7b46f721bd2cbbccc5b6c
Pedro Afonso <apolaropeo@gmail.com>
2021-05-07 20:47:48 -0300

    number analyzer

--- challenge/ch007/script.js
+++ challenge/ch007/script.js
@@ -21,8 +21,40 @@ function inList(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inList(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `add ${num.value} value.`
+        list.appendChild(item)
+        exit.innerHTML = ''
     } else {
         window.alert('Invalid value or already found in the list.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finish() {
+    if (valores.length == 0) {
+        window.alert('add value before finish!')
+    } else {
+        let tot = valores.length
+        let bigger = valores[0]
+        let smaller = valores[0]
+        let sum = 0
+        let media = 0
+        for(let pos in valores) {
+            sum += valores[pos]
+            if(valores[pos] > bigger)
+            bigger = valores[pos]
+            if(valores[pos] < smaller)
+            smaller = valores[pos]
+        }
+        media = sum / tot
+        exit.innerHTML = ''
+        exit.innerHTML += `<p>Altogether there is ${tot} registered numbers.</p>`
+        exit.innerHTML += `<p>The bigger value is ${bigger}.</p>`
+        exit.innerHTML += `<p>The smaller value is ${smaller}.</p>`
+        exit.innerHTML += `<p>The sum of all values is ${sum}.</p>`
+        exit.innerHTML += `<p>The media of all entered numbers is ${media}.</p>`
     }
 }
